90cb4a89f7ce4de150b59b1c46d4eab889e1b5cb
willcordeiro <willcordeiro800@gmail.com>
2025-10-05 19:06:04 -0300

    feat: added ether context, types

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { WalletProvider } from "@/context/WalletContext";
+import { EtherscanProvider } from "@/context/EtherscanContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <WalletProvider>{children}</WalletProvider>
+        <WalletProvider>
+          {" "}
+          <EtherscanProvider>{children} </EtherscanProvider>
+        </WalletProvider>
       </body>
     </html>
   );
---
+++ src/types/transaction.ts
@@ -0,0 +1,20 @@
+export interface EtherscanTransaction {
+  blockNumber: string;
+  timeStamp: string;
+  hash: string;
+  nonce: string;
+  blockHash: string;
+  from: string;
+  contractAddress: string;
+  to: string;
+  value: string;
+  tokenName: string;
+  tokenSymbol: string;
+  tokenDecimal: string;
+  transactionIndex: string;
+  gas: string;
+  gasPrice: string;
+  gasUsed: string;
+  cumulativeGasUsed: string;
+  confirmations: string;
+}
